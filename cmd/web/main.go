f978e9ab5d4fda0feff961c28678895cea30ed3f
richwynmorris <rwmorris.bjj@gmail.com>
2022-03-09 17:59:46 +0000

    added flag feature to specify the port the application should run on

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command-line flar with the name 'addr'
+	// The value of the flag will be stored in the addr variable at runtime
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// parses the command line flag. Needs to be called before using or
+	// will use the defaul value
+	flag.Parse()
+
 	// Use the http.NewServeMux() function to initialize a new servemux/router,
 	// then register the home function as the handler for the "/" URL pattern
 	mux := http.NewServeMux()
@@ -21,11 +30,11 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on port 4000")
+	log.Printf("Starting server on port %s", *addr)
 
 	// ListenAndServe function starts a new web server. It takes two paramters, the TCP
 	// network address and the router
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	// If the ListenAndServe function returns an error, you can log the error by passing it
 	// to the fatal function and exiting.
 	log.Fatal(err)
