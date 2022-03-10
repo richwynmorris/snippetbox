c74d26cd10b5dc542a0dfa8df158c38afa1fe486
richwynmorris <rwmorris.bjj@gmail.com>
2022-03-10 10:12:53 +0000

    abstracted routes to seperate function which adds required routes to new router and returns new router

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,36 +19,27 @@ func main() {
 	// parses the command line flag. Needs to be called before using or
 	// will use the defaul value
 	flag.Parse()
-
 	// Use log.New() to create a logger. Takes 3 params - destination, prefix and flags
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	// For error logs, use stderr as dest and log.Lshortfile
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	//-------------------------------------------------------------------------------------
+
+	// Initializes new application struct - has errorLog and InfoLog dependencies
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	// Use the http.NewServeMux() function to initialize a new servemux/router,
-	// then register the home function as the handler for the "/" URL pattern
-	mux := http.NewServeMux()
-	// Default path treats patter "/" as a catch all. All requests to the server will
-	//handled by the home function
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server which serves files out of the static directory
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Initiazlise new http.Server struct - add address, error log and handler
+	// Initiazlise new http.Server struct - add address, error log and handler dependencies
 	server := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
+
+	//-------------------------------------------------------------------------------------
 
 	infoLog.Printf("Starting server on port %s", *addr)
 
---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServerMux {
+	// Use the http.NewServeMux() function to initialize a new servemux/router,
+	// then register the home function as the handler for the "/" URL pattern
+	mux := http.NewServeMux()
+	// Default path treats patter "/" as a catch all. All requests to the server will
+	//handled by the home function
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	// Create a file server which serves files out of the static directory
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
