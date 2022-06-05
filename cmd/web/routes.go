7e8a319cabafe5700b33a0d665fc5d0faf69ed5f
richwynmorris <richwynmorris@protonmail.com>
2022-06-05 11:15:16 +0100

    refactor: implemented http-router package to support clean URLS and method-based routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,22 +3,29 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	// Initialize a new router.
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	// Use Alice package to create middleware chain and add readability.
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	// Return the standardMiddleware chain followed by servemux.
-	return standardMiddleware.Then(mux)
+	// Return the standardMiddleware chain followed by http-router.
+	return standardMiddleware.Then(router)
 }
