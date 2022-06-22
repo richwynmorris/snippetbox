404d03b657b245d9166d26cdfd83bfb80852afc0
richwynmorris <richwynmorris@protonmail.com>
2022-06-22 09:35:43 +0100

    feat: updated template cache to use embedded html files from file system

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -43,7 +43,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		patterns := []string{
 			"html/base.tmpl.html",
 			"html/partials/*.tmpl",
-			page
+			page,
 		}
 
 		// Parse base template
