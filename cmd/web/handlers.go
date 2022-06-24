7cd155d7b079b3e0f442ab54796b9478d32d0762
richwynmorris <richwynmorris@protonmail.com>
2022-06-24 09:02:15 +0100

    fix: use permittedValue function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -93,7 +93,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "this cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "title cannot have more than 100 chars")
 	form.CheckField(validator.NotBlank(form.Content), "content", "this cannot be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "must be either 1, 7 or 365")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "must be either 1, 7 or 365")
 
 	// Check if any FieldErrors have been raised by the validator.
 	if !form.Valid() {
