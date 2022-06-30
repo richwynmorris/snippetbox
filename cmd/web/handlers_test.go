619539dbf990b127542211ddc87f0f8e4867bca4
richwynmorris <richwynmorris@protonmail.com>
2022-06-30 09:29:00 +0100

    refactor: implemented helper functions into test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,42 +1,25 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"richwynmorris.co.uk/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
 	// Initialize a new test server to run the end to end test.
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	// Generate a test client and make a request to the test server and the pin endpoint.
-	result, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	// Generate a test client and make a request to the test server and the ping endpoint.
+	code, _, body := ts.get(t, "/ping")
 
 	// Check that result of the test has a status of 200.
-	assert.Equal(t, result.StatusCode, http.StatusOK)
-
-	defer result.Body.Close()
-	body, err := io.ReadAll(result.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
+	assert.Equal(t, code, http.StatusOK)
 
 	// Check that the response contains an OK message in its body.
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, body, "OK")
 }
